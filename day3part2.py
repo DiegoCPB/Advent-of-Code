44fcdbdb64678f472a7be109dd9186f8dc0fe6b9
DiegoCPB <diego.chou@gmail.com>
2025-12-03 21:06:41 +0100

    optimisation day3part2.py

--- day3part2.py
+++ day3part2.py
@@ -8,12 +8,13 @@ def total_joltage(filepath):
                 bank = [int(s) for s in line]
                 bank_joltage = ''
                 for i in range(n,0,-1):
-                    inds = sorted(range(len(bank)-(i-1)),key=bank.__getitem__,reverse=True)
-                    bank_joltage += str(bank[inds[0]])
-                    bank = bank[inds[0]+1:]
+                    j = len(bank)-(i-1)
+                    ind = bank.index(max(bank[:j]))
+                    bank_joltage += str(bank[ind])
+                    bank = bank[ind+1:]
                 counter += int(bank_joltage)
     return counter
 
 if __name__ == "__main__":
-    filepath = 'inputs/day3.txt'
+    filepath ='inputs/day3.txt'
     print(total_joltage(filepath))
